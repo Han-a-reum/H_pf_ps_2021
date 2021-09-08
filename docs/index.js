33efb9c898806d9fc10f6b04aa8e2473004da7e9
Han-a-reum <hanar0323@naver.com>
2021-09-08 23:52:19 +0900

    배치중1

--- docs/index.js
+++ docs/index.js
@@ -3,3 +3,14 @@ $('body').niceScroll({
   scrollspeed: 10,
   cursorcolor : "#424242;"
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 8,
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
